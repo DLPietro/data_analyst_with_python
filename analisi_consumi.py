5da54b22e88dff2e5d558112623fa7ee06b0844d
DLPietro <90868226+DLPietro@users.noreply.github.com>
2025-07-09 10:17:39 +0200

    Update analisi_consumi.py

--- analisi_consumi.py
+++ analisi_consumi.py
@@ -1,10 +1,12 @@
 # Installazioine ed importazione librerie
 !pip install pandas matplotlib seaborn
-import pandas as pd, matplotlib.pyplot as plt, seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Cariacamento dataset
-df = pd.read_csv("consumi_alimentari_settimana.csv")
+df = pd.read_csv("calorie_alimentari_settimana.csv")
 
 # Visualizzazione dati (prime ed ultime righe)
 print("Primi ed ultimi dati del dataset:")
-print(df.head(), df.tail())
+print(df.head())
