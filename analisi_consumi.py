7c118eabf4f4df7bdccc66a4c18c56507333da42
DLPietro <90868226+DLPietro@users.noreply.github.com>
2025-07-09 11:13:44 +0200

    Update analisi_consumi.py

--- analisi_consumi.py
+++ analisi_consumi.py
@@ -4,9 +4,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Cariacamento dataset
+# Cariacamento dataset
 df = pd.read_csv("calorie_alimentari_settimana.csv")
 
 # Visualizzazione dati (prime ed ultime righe)
 print("Primi ed ultimi dati del dataset:")
-print(df.head(); df.tail())
+print(df.head(), df.tail())
+
+# Calcolo totale giornaliero di calorie
+df['Totale']=df[['Colazione (kcal)', 'Pranzo (kcal)', 'Cena (kcal)', 'Snack (kcal)']].sum(axis=1)
+
+# Calcolo Media
+media_calorie = df['Totale'].mean()
+print(f"\nMedia di calorie giornaliere: {calorie_medie:.0f} kcal")
