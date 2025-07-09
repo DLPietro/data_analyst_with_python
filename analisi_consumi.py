b1894eb1590bc52bf69d24a3b94addf96ada6087
DLPietro <90868226+DLPietro@users.noreply.github.com>
2025-07-09 12:11:12 +0200

    Update analisi_consumi.py

--- analisi_consumi.py
+++ analisi_consumi.py
@@ -17,3 +17,21 @@ df['Totale']=df[['Colazione (kcal)', 'Pranzo (kcal)', 'Cena (kcal)', 'Snack (kca
 # Calcolo Media
 media_calorie = df['Totale'].mean()
 print(f"\nMedia di calorie giornaliere: {calorie_medie:.0f} kcal")
+
+# Calcolo del totale settimanale per ciscun tipo di pasto
+colazioni_totali = df['Colazione (kcal)'].sum()
+pranzi_totali = df['Pranzo (kcal)'].sum()
+cene_totali = df['Cena (kcal)'].sum()
+spuntini_totali = df['Spuntino (kcal)'].sum()
+
+# Preaparazione dati per il grafico a torta
+labels = ['Colazione', 'Pranzo', 'Cena', 'Spuntino']
+sizes = [colazioni_totali, pranzi_totali, cene_totali, spuntini_totali]
+colors = sns.color_palette('pastel')
+
+# Creazione Grafico a Torta
+plt.figure(figsize=(8, 8))
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+plt.title("Distribuzione delle calorie settimanali")
+plt.axis('equal')  # Grafico Circolare
+plt.show()
